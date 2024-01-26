17e537b57d3e664eaf6bb3de832f6e2861e18653
andres <andresfelipelozano03@gmail.com>
2024-01-26 14:39:16 -0500

    moved SUICoordinatorExample to Example folder

---
+++ Examples/SUICoordinatorExample/SUICoordinatorExample/App/SUICoordinatorExampleApp.swift
@@ -0,0 +1,21 @@
+//
+//  SUICoordinatorExampleApp.swift
+//  SUICoordinatorExample
+//
+//  Created by Andres Lozano on 26/01/24.
+//
+
+import SwiftUI
+
+@main
+struct SUICoordinatorExampleApp: App {
+    @UIApplicationDelegateAdaptor private var appDelegate: AppDelegate
+    
+    var body: some Scene {
+        WindowGroup {
+            if let view = appDelegate.mainCoodinator?.view {
+                AnyView(view)
+            }
+        }
+    }
+}
---
+++ Examples/SUICoordinatorExample/SUICoordinatorExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SUICoordinatorExample
+//
+//  Created by Andres Lozano on 26/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
