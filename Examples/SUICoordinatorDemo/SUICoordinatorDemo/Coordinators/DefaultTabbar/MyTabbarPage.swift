4bfeb4b985ab77a5dc4f5a8034d111aa8170caf5
felilo <10853689+felilo@users.noreply.github.com>
2024-01-26 12:24:00 -0500

    Moved demo project to Example folder (#3)
    * moves project demo to Examples folder and updates Readme

---
+++ Examples/SUICoordinatorDemo/SUICoordinatorDemo/Coordinators/DefaultTabbar/MyTabbarPage.swift
@@ -0,0 +1,76 @@
+//
+//  MyTabbarPage.swift
+//
+//  Copyright (c) Andres F. Lozano
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+import SUICoordinator
+import SwiftUI
+
+
+public enum MyTabbarPage: TabbarPage, CaseIterable {
+    
+    case first
+    case second
+    
+    // ---------------------------------------------------------
+    // MARK: TabbarPage
+    // ---------------------------------------------------------
+    
+    public var badge: String? {
+        switch self {
+            case .first: "new"
+            case .second: "10"
+        }
+    }
+    
+    @ViewBuilder
+    public var icon: (any View) {
+        switch self {
+            case .first: Image.init(systemName: "homekit")
+            case .second: Image.init(systemName: "gear")
+        }
+    }
+    
+    @ViewBuilder
+    public var title: (any View) {
+        switch self {
+            case .first: Text("first")
+            case .second: Text("second")
+        }
+    }
+    
+    public var position: Int {
+        switch self {
+            case .first: return 0
+            case .second: return 1
+        }
+    }
+    
+    public func coordinator() -> (any CoordinatorType) {
+        switch self {
+            case .first:
+                return HomeCoordinator()
+            case .second:
+                return TabbarFlowCoordinator()
+        }
+    }
+}
