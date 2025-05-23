07a09183feed989e06fb0b686fc16f2f7478511a
andres <andresfelipelozano03@gmail.com>
2025-05-23 14:42:11 -0500

    Changes the presentation mode for sheet from background to overlay modifier

--- Sources/SUICoordinator/SheetCoordinator/SheetCoordinatorView.swift
+++ Sources/SUICoordinator/SheetCoordinator/SheetCoordinatorView.swift
@@ -56,14 +56,14 @@ struct SheetCoordinatorView: ViewModifier {
     @ViewBuilder
     func body(content: Content) -> some View {
         content
-            .background {
+            .overlay {
                 VStack {
                     SheetView(
                         index: index,
                         items: $coordinator.items,
-                        content: buildContent,
                         transitionStyle: coordinator.lastPresentationStyle,
                         animated: coordinator.animated ?? true,
+                        content: buildContent,
                         onDismiss: onDissmis,
                         onDidLoad: onDidLoad
                     )
